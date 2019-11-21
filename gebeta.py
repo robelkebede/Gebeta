6f67f88e5adcf3dab28cb9b09cbc05685c4ec1e3
Robel kebede <robelkebede44@gmail.com>
2019-11-21 10:01:02 +0300

    fixed board state

--- gebeta.py
+++ gebeta.py
@@ -5,14 +5,16 @@ class Gebeta():
 
         self.score_player_0 = 0
         self.score_player_1 = 0
-        self.board_position = None
+        self.board_position = None 
 
     def board(self):
         
-        self.board_position = np.array([[4,4,4,4,4,4],
-                [4,4,4,4,4,4]])
-        
+        #self.board_position = np.array([[4-3,4-3,4-3,4-3,4-3,4-3],[4-3,4-3,4-3,4-3,4-3,4-3]])
+        self.board_position = np.array([[4,4,4,4,4,4],[4,4,4,4,4,4]])
         return self.board_position
+
+    def update(self,board):
+        self.board_position = board
 
     def moves(self,player_id):
 
@@ -39,13 +41,13 @@ class Gebeta():
 
     def end_game(self,board):
 
-        board = self.board()
-        p0 = all(1 ==i for i in board[0])
-        p1 = all(1 ==i for i in board[1])
+        p0 = all(0 ==i for i in board[0])
+        p1 = all(0 ==i for i in board[1])
 
-        if p0 or p1 is True:
-            print("Score p0 {} p2 {}".format(self.score_player_0,self.score_player_1))
-            return p0 
+        if p0 or p1 == True:
+            return True
+        else:
+            return False
 
 
     def start_pos(self,player_id,pos_x,pos_y):
@@ -61,62 +63,82 @@ class Gebeta():
         return move
 
 
-    def play(self,player_id,pos_x,pos_y):
+    def play(self,board_position,player_id,pos_x,pos_y,p0s=0,p1s=0):
         #acesses denied when player is trying to acsses another position
         #move starting point
         game_pos = self.start_pos(player_id,pos_x,pos_y)
-        hole_value = self.board_position[game_pos[0][0],game_pos[0][1]]
-        self.board_position[game_pos[0][0],game_pos[0][1]] = 0
+        hole_value = board_position[game_pos[0][0],game_pos[0][1]]
+        board_position[game_pos[0][0],game_pos[0][1]] = 0
 
-        local_score_0 = 0
-        local_score_1 = 0
-
-        print(player_id)
+        print([hole_value,board_position])
+        num_iter = 0
 
         for i in range(1,10000):
-            
+            num_iter += 1 
             if hole_value == 0:
                 break #end game
             
             if game_pos[i][0] == 9:
 
                 if player_id == 0:
-                    self.score_player_0 += 1
+                    p0s += 1
                     hole_value -=1
                 else:
-                    local_score_1 += 1
-                    self.score_player_1 += 1
+                    p1s += 1
                     hole_value -=1
 
             else:
 
                 if hole_value == 1:
-                    hole_value = self.board_position[game_pos[i][0],game_pos[i][1]] +1
-                    self.board_position[game_pos[i][0],game_pos[i][1]] =0
+                    hole_value = board_position[game_pos[i][0],game_pos[i][1]] +1
+                    board_position[game_pos[i][0],game_pos[i][1]] =0
                 else:
-                    self.board_position[game_pos[i][0],game_pos[i][1]] +=1
+                    board_position[game_pos[i][0],game_pos[i][1]] +=1
                     hole_value -= 1
 
-        
-        return [self.board_position,self.score_player_0,self.score_player_1]
+        return  board_position,p0s,p1s,num_iter,game_pos
 
 
     
     
 def main():
+
     ge = Gebeta()
     board = ge.board()
-    print(board)
+
+    s0 = 0
+    s1 = 0
+
     while True:
-        if ge.end_game(board):
-            break
-        
+                
+           
         player_input = input()
-        board_position,ls0,ls1 = ge.play(int(player_input[0]),int(player_input[1]),int(player_input[2]))
-        #board_position,ls0,ls1 = ge.play(0,0,5)
+        board_position,p0s,p1s,num_iter,pos = ge.play(board,int(player_input[0]),
+                int(player_input[1]),int(player_input[2]),s0,s1)
+
+        s0 = p0s
+        s1 = p1s
+        print("#######################################################################") 
         print("Player ",int(player_input[0])) 
-        print(board_position)
+        print("num Iter",num_iter)
+        #print("Pos",pos )
+        board = board_position
+
+        print(board)
+        
+        ls0 = s0
+        ls1 = s1
+        
+        print(np.sum(board)+ls0+ls1)
         print({"score player 0 ":ls0,"score player 1 ":ls1})
+
+        if ge.end_game(board):
+            print("Game ended")
+            break
+
+        print("########################################################################")
+        
+
 
 
 if __name__ == "__main__":
