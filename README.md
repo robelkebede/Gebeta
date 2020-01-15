94685c008cb0510d0b2bc1c56f8ae3dd3408f06c
INC SERVER1 <robelkebede44@gmai.com>
2020-01-15 17:31:58 +0300

    uncleaned deep q implementaion

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 
 * The Oldest Board Game In The World
 * train a reinforcement learning agent to play Gebeta
-* Tkinter gui for the Board
-* deep reinforcement learning
+* Tkinter gui for the Board (soon)
+* deep reinforcement learning (soon)
 
 
 [RULES] ("https://www.youtube.com/watch?v=c62_xn8XC2g")
@@ -17,7 +17,6 @@ git clone http://github.com/robelkebede/gebeta.git
 cd ./gebeta	
 
 ./gebeta.py
-
 
 
 ## Contributing
--- agent.py
+++ agent.py
@@ -7,11 +7,11 @@ from gebeta import Gebeta
 from tqdm import tqdm
 from termcolor import colored
 import matplotlib.pyplot as plt
+from dqn import DQN
 
 class Agent():
     def __init__(self,num):
         self.num = num
-        self.model = joblib.load("./model/gebeta_model_1")
         self.rl_model = np.load("./data/data10000x.npy")
 
     def action(self,player_id):
@@ -21,10 +21,6 @@ class Agent():
         else:
             return [1,random.randint(0,5)]
     
-    def neural_action(self,action):
-        predicted_action = self.model.predict(action.reshape(1,-1))
-        
-        return predicted_action
 
     def reward(self,r,j):
         if r == 0 :
@@ -91,13 +87,14 @@ class Agent():
 
                 j+=1
 
-                if gebeta.end_game(board) or j>60:
-                    Done = True
-                    board = gebeta.board()
-                    print(colored("GAME ENDED","red"),"who won ",i,j)
-                    #time.sleep(0.5)
-                    break
-        
+
+            if gebeta.end_game(board) or j>60:
+                Done = True
+                board = gebeta.board()
+                print(colored("GAME ENDED","red"),"who won ",i,j)
+                #time.sleep(0.5)
+                break
+    
         print(len(r_list))
         plt.plot(r_list)
         #np.save("data100000x",Q)
@@ -110,6 +107,73 @@ class Agent():
         
         return np.argmax(value)
 
+
+    def play_deep_rl(self,gebeta):
+
+        print(DQN)
+        num_ep = 1
+        r_list = []
+        for i in range(num_ep):
+
+                r_all = 0 #reward (ALL)
+                j = 0
+                Done = False
+                board = gebeta.board()
+
+                while not Done:
+
+                    s = board
+
+                    dqn_agent = DQN()
+
+                    if j%2 == 0:
+
+                        action = dqn_agent.act(s)
+
+                        s1,r,_,_,_ = gebeta.play(s,0,0,a)
+
+
+                        reward = r if not Done else -20
+                        print(reward)
+                        new_state = s1.reshape(2,6)
+                        dqn_agent.remember(s, action,r, s1, Done)
+
+                        r_all+= r
+
+
+                        r_list.append(r_all)
+
+                        dqn_agent.replay()
+                        dqn_agent.target_train()
+
+
+                    else:
+                        print(s)
+                        action = np.random.randint(5)
+                        s1,_,r,_,_ = gebeta.play(s,1,1,action)
+
+                        print("Random",s1)
+                        print("action",action)
+                        print("reward",r)
+                        print("++++++++++++++++++++++++++++++++++++++++++")
+                        s = s1
+
+                    j+=1
+
+                    if gebeta.end_game(board) or j>60:
+                        Done = True
+                        board = gebeta.board()
+                        print(colored("GAME ENDED","red"),"who won ",i,j)
+                        #time.sleep(0.5)
+                        break
+
+        print(len(r_list))
+        plt.plot(r_list)
+        #np.save("data100000x",Q)
+        plt.show() 
+
+
+
         
 
 
@@ -118,7 +182,9 @@ if __name__ == "__main__":
     board_position = np.array([[4,4,4,4,4,4],[4,4,4,4,4,4]])
     a = Agent(2)
     gebeta = Gebeta()
-    a.rl(gebeta)
+    #a.rl(gebeta)
     #print(a.play_rl(board_position))
+
+    a.play_deep_rl(gebeta) 
 
 
